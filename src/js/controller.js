7d2f94db10f6470b1a0dcb4b3578e99f42189840
bruno de clercq <brunojl.declercq@gmail.com>
2023-07-04 16:02:16 +0200

    helpers and config

--- src/js/controller.js
+++ src/js/controller.js
@@ -7,14 +7,6 @@ import 'regenerator-runtime/runtime';
 
 const recipeContainer = document.querySelector('.recipe');
 
-const timeout = function (s) {
-  return new Promise(function (_, reject) {
-    setTimeout(function () {
-      reject(new Error(`Request took too long! Timeout after ${s} second`));
-    }, s * 1000);
-  });
-};
-
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
@@ -23,7 +15,7 @@ const controlRecipes = async function () {
   try {
     //using the # to
     const id = window.location.hash.slice(1);
-    console.log(id);
+    // console.log(id);
 
     if (!id) return;
     recipeView.renderSpinner();
