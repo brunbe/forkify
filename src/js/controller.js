d173e552801d86823edb72992a421548c0587929
bruno de clercq <brunojl.declercq@gmail.com>
2023-07-04 10:57:00 +0200

    MVC refactor

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,3 +1,6 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+
 // import icons from '../img/icons.svg'; // parcel 1
 import icons from 'url:../img/icons.svg'; // parcel 2
 import 'core-js/stable';
@@ -34,27 +37,11 @@ const showRecipe = async function () {
     console.log(id);
 
     if (!id) return;
+    renderSpinner(recipeContainer);
 
     //1. loading recipe the recipe
-    renderSpinner(recipeContainer);
-    const res = await fetch(
-      `https://forkify-api.herokuapp.com/api/v2/recipes/${id}`
-    );
-    const data = await res.json();
-    if (!res.ok) throw new Error(`${data.message} (${res.status})`);
-    console.log(res, data);
-    let { recipe } = data.data;
-    recipe = {
-      id: recipe.id,
-      title: recipe.title,
-      publisher: recipe.publisher,
-      sourceUrl: recipe.source_url,
-      image: recipe.image_url,
-      servings: recipe.servings,
-      cookingTime: recipe.cooking_time,
-      ingredients: recipe.ingredients,
-    };
-    console.log(recipe);
+    await model.loadRecipe(id);
+    const { recipe } = model.state;
 
     //2. Rendering recipe:
     const markup = `<figure class="recipe__fig">
