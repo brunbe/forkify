bdbd972ec9aea0f8bbb0851d9f6dce30b3b4ee26
bruno de clercq <brunojl.declercq@gmail.com>
2023-07-06 14:48:54 +0200

    implementing search results

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,12 +1,11 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
+import searchView from './views/searchView.js';
 // import addHandlerRender from './views/recipeView.js';
 
 // import icons from '../img/icons.svg'; // parcel 1
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
-
-const recipeContainer = document.querySelector('.recipe');
 
 // https://forkify-api.herokuapp.com/v2
 
@@ -31,8 +30,27 @@ const controlRecipes = async function () {
   }
 };
 
+const controlSearchResults = async function () {
+  try {
+    //1) Get search query and clear search bar;
+    const query = searchView.getQuery();
+    if (!query) return;
+
+    //2) Load search results
+    await model.loadSearchResults(query);
+
+    // 3) Render results
+    console.log(model.state.search.results);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+controlSearchResults();
+
 const init = function () {
   recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
 };
 
 init();
